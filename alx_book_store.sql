fdd14121d5930792d25b75f138e36ffb87e0a088
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-10-19 21:04:57 +0000

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,12 +1,16 @@
-CREATE DATABASE alx_book_store;
+-- Create the alx_book_store database if it does not already exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
+-- Use the alx_book_store database
 USE alx_book_store;
 
+-- Create the Authors table
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
+-- Create the Books table
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -16,6 +20,7 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
+-- Create the Customers table
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -23,6 +28,7 @@ CREATE TABLE Customers (
     address TEXT
 );
 
+-- Create the Orders table
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -30,6 +36,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
+-- Create the Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
